249de42a8e8f742a09d3eebcdc56527a78efead8
Nayab-Sarfaraj <nayabsarfaraj@gmail.com>
2025-06-04 21:56:19 +0530

    fool

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,7 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
+console.log(process.env.HOST);
 const pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
